5b8ce1fb00e6bba2cedf2ee171d98208706b21b0
Mosblinker <msteier2.0@gmail.com>
2025-09-09 13:23:52 -0500

    Added some methods to the config class
    I've added some utilities methods to the config class, along with the preference nodes.

--- src/iconifier/IconifierConfig.java
+++ src/iconifier/IconifierConfig.java
@@ -4,6 +4,17 @@
  */
 package iconifier;
 
+import config.ConfigUtilities;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.prefs.Preferences;
+import javax.swing.JFileChooser;
+import utils.SwingExtendedUtilities;
+
 /**
  *
  * @author Mosblinker
@@ -48,4 +59,198 @@ public class IconifierConfig {
      */
     public static final String FILE_CHOOSER_FILE_FILTER_KEY = 
             "FileFilter";
+    /**
+     * This is a preference node to store the settings for this program.
+     */
+    private final Preferences node;
+    /**
+     * 
+     */
+    private final Map<JFileChooser, Preferences> fcNodes;
+    /**
+     * 
+     * @param node 
+     */
+    public IconifierConfig(Preferences node){
+        this.node = Objects.requireNonNull(node);
+        fcNodes = new HashMap<>();
+    }
+    /**
+     * 
+     * @return 
+     */
+    public Preferences getPreferences(){
+        return node;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public Map<JFileChooser, Preferences> getFileChooserPreferenceMap(){
+        return fcNodes;
+    }
+    /**
+     * 
+     * @param fc
+     * @param name
+     */
+    public void addFileChooser(JFileChooser fc, String name){
+        if (!fcNodes.containsKey(fc))
+            fcNodes.put(fc, getPreferences().node(name));
+    }
+    /**
+     * 
+     * @param fc
+     * @return 
+     */
+    public Preferences getFileChooserPreferences(JFileChooser fc){
+        return fcNodes.get(fc);
+    }
+    /**
+     * 
+     * @param key
+     * @param defaultValue
+     * @return 
+     */
+    public Dimension getDimension(String key, Dimension defaultValue){
+        return ConfigUtilities.getDimension(node, key, defaultValue);
+    }
+    /**
+     * 
+     * @param key
+     * @return 
+     */
+    public Dimension getDimension(String key){
+        return getDimension(key,null);
+    }
+    /**
+     * 
+     * @param key
+     * @param width
+     * @param height 
+     */
+    public void putDimension(String key, int width, int height){
+        ConfigUtilities.putDimension(node, key, width, height);
+    }
+    /**
+     * 
+     * @param key
+     * @param dim 
+     */
+    public void putDimension(String key, Dimension dim){
+        ConfigUtilities.putDimension(node, key, dim);
+    }
+    /**
+     * 
+     * @param key
+     * @param comp 
+     */
+    public void putDimension(String key, Component comp){
+        ConfigUtilities.putDimension(node, key, comp);
+    }
+    /**
+     * 
+     * @param key
+     * @param defaultValue
+     * @return 
+     */
+    public Rectangle getRectangle(String key, Rectangle defaultValue){
+        return ConfigUtilities.getRectangle(node, key, defaultValue);
+    }
+    /**
+     * 
+     * @param key
+     * @return 
+     */
+    public Rectangle getRectangle(String key){
+        return getRectangle(key,null);
+    }
+    /**
+     * 
+     * @param key
+     * @param x
+     * @param y
+     * @param width
+     * @param height 
+     */
+    public void putRectangle(String key, int x, int y, int width, int height){
+        ConfigUtilities.putRectangle(node, key, x, y, width, height);
+    }
+    /**
+     * 
+     * @param key
+     * @param width
+     * @param height 
+     */
+    public void putRectangle(String key, int width, int height){
+        putRectangle(key,0,0,width,height);
+    }
+    /**
+     * 
+     * @param key
+     * @param value 
+     */
+    public void putRectangle(String key, Rectangle value){
+        ConfigUtilities.putRectangle(node, key, value);
+    }
+    /**
+     * 
+     * @param key
+     * @param comp 
+     */
+    public void putRectangle(String key, Component comp){
+        ConfigUtilities.putRectangle(node, key, comp);
+    }
+    /**
+     * 
+     * @param node
+     * @param key
+     * @param defaultFile
+     * @return 
+     */
+    protected File getFile(Preferences node, String key, File defaultFile){
+            // Get the name of the file from the preference node, or null
+        String name = node.get(key, null);
+            // If there is no value set for that key
+        if (name == null)
+            return defaultFile;
+        return new File(name);
+    }
+    /**
+     * 
+     * @param key
+     * @param defaultFile
+     * @return 
+     */
+    public File getFile(String key, File defaultFile){
+        return getFile(getPreferences(),key,defaultFile);
+    }
+    /**
+     * 
+     * @param key
+     * @return 
+     */
+    public File getFile(String key){
+        return getFile(key,null);
+    }
+    /**
+     * 
+     * @param node
+     * @param key
+     * @param value 
+     */
+    protected void putFile(Preferences node, String key, File value){
+        if (value == null)
+            node.remove(key);
+        else
+            node.put(key, value.toString());
+    }
+    /**
+     * 
+     * @param key
+     * @param value 
+     */
+    public void putFile(String key, File value){
+        putFile(getPreferences(),key,value);
+    }
 }
